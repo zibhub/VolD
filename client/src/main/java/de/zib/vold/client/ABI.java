f745b5cde616ed595460f60655cecaeb7a6bd86f
Jörg Bachmann <bachmann@zib.de>
2011-11-11 14:10:59 +0100

    Initial check-in
    This ABI helps debugging

---
+++ client/src/main/java/de/zib/vold/client/ABI.java
@@ -0,0 +1,26 @@
+
+package de.zib.vold.client;
+
+import de.zib.vold.common.VoldException;
+import de.zib.vold.common.Key;
+
+import de.zib.vold.client.RESTClient;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ABI
+{
+}
