b1ff53222a06fa71f2194a633fe63189a257090f
Jörg Bachmann <bachmann@zib.de>
2011-12-21 17:30:41 +0100

    Rmoved unnecessary code and retabed RESTController

--- server/src/main/java/de/zib/vold/userInterface/RESTController.java
+++ server/src/main/java/de/zib/vold/userInterface/RESTController.java
@@ -43,199 +43,199 @@ import java.util.*;
 //@RequestMapping( "*" )
 public class RESTController
 {
-        protected final Logger log = LoggerFactory.getLogger( this.getClass() );
-        private static String defaulttype = "list";
+    protected final Logger log = LoggerFactory.getLogger( this.getClass() );
 
-        private Frontend frontend;
-        private String enc = "utf-8";
+    private Frontend frontend;
+    private String enc = "utf-8";
 
-        private void checkState()
+    private void checkState()
+    {
+        if( null == frontend )
         {
-                if( null == frontend )
-                {
-                        throw new IllegalStateException( "Tried to operate on REST controller while it had not been initialized yet. Set a frontend first!" );
-                }
+            throw new IllegalStateException( "Tried to operate on REST controller while it had not been initialized yet. Set a frontend first!" );
+        }
+    }
+
+    /**
+     * Handles Post requests.
+     *
+     * This method is used by clients to submit new keys, refresh their registration or delete them.
+     *
+     * @param clientIpAddress The ip of the sending client, it's extracted from the request itself.
+     * @param args The URL arguments of the request.
+     * @param argsbody The POST body arguments of the request.
+     * @param request Request informations
+     * @return A map of keys with its lifetime, whereas the livetime is zero if an error for that key occured.
+     */
+    @RequestMapping( method = RequestMethod.POST )
+    public ResponseEntity< Map< String, String > > post(
+            @ModelAttribute("clientIpAddress") String clientIpAddress,
+            @RequestParam MultiValueMap< String, String > args,
+            @RequestBody MultiValueMap< String, String > argsbody,
+            HttpServletRequest request)
+    {
+
+        // guard
+        {
+            if( argsbody != null )
+                log.debug( "POST: " + args.toString() + " AND " + argsbody.toString() );
+            else
+                log.debug( "POST: " + args.toString() );
+
+            checkState();
         }
 
-        /**
-        * Handles Post requests.
-        *
-        * This method is used by clients to submit new keys, refresh their registration or delete them.
-        *
-        * @param clientIpAddress The ip of the sending client, it's extracted from the request itself.
-        * @param args The URL arguments of the request.
-        * @param argsbody The POST body arguments of the request.
-        * @return A map of keys with its lifetime, whereas the livetime is zero if an error for that key occured.
-        */
-        @RequestMapping( method = RequestMethod.POST )
-        public ResponseEntity< Map< String, String > > post(
-                        @ModelAttribute("clientIpAddress") String clientIpAddress,
-                        @RequestParam MultiValueMap< String, String > args,
-                        @RequestBody MultiValueMap< String, String > argsbody,
-                        HttpServletRequest request)
-        {
+        Map< String, String > invalidKeys = new HashMap< String, String >();
 
-                // guard
+        // get actual scope
+        String scope;
+        {
+            scope = request.getRequestURI();
+            String removepath = request.getContextPath() + request.getServletPath();
+
+            scope = scope.substring( removepath.length(), scope.length() );
+        }
+
+        // merge args to argsbody
+        {
+            if( null == argsbody )
+            {
+                if( null == args )
                 {
-                        if( argsbody != null )
-                                log.debug( "POST: " + args.toString() + " AND " + argsbody.toString() );
+                    log.warn( "Got a totally empty request from " + clientIpAddress + "." );
+                    return new ResponseEntity< Map < String, String > >( invalidKeys, HttpStatus.OK );
+                }
+
+                argsbody = args;
+            }
+            else if( null != args )
+            {
+                argsbody.putAll( args );
+            }
+        }
+
+        // process each key
+        {
+            for( Map.Entry< String, List< String > > entry: argsbody.entrySet() )
+            {
+                URIKey urikey;
+                String source;
+                Key k;
+
+                // build key
+                {
+                    urikey = URIKey.fromURIString( entry.getKey(), enc );
+
+                    File path_correction = new File( scope + "/" + urikey.getKey().get_scope() );
+
+                    k = new Key(
+                            path_correction.getPath(),
+                            urikey.getKey().get_type(),
+                            urikey.getKey().get_keyname()
+                    );
+
+                    if( null == urikey.getSource() )
+                    {
+                        source = clientIpAddress;
+                    }
+                    else
+                    {
+                        source = urikey.getSource();
+                    }
+                }
+
+                // handle write request for that key
+                {
+                    try
+                    {
+                        if( urikey.isDelete() )
+                        {
+                            frontend.delete( source, k );
+                        }
+                        else if( urikey.isRefresh() )
+                        {
+                            frontend.refresh( source, k );
+                        }
                         else
-                                log.debug( "POST: " + args.toString() );
-
-                        checkState();
-                }
-
-                Map< String, String > invalidKeys = new HashMap< String, String >();
-
-                // get actual scope
-                String scope;
-                {
-                        scope = request.getRequestURI();
-                        String removepath = request.getContextPath() + request.getServletPath();
-
-                        scope = scope.substring( removepath.length(), scope.length() );
-                }
-
-                // merge args to argsbody
-                {
-                        if( null == argsbody )
                         {
-                                if( null == args )
-                                {
-                                        log.warn( "Got a totally empty request from " + clientIpAddress + "." );
-                                        return new ResponseEntity< Map < String, String > >( invalidKeys, HttpStatus.OK );
-                                }
-
-                                argsbody = args;
+                            frontend.insert( source, k, new HashSet< String >( entry.getValue() ) );
                         }
-                        else if( null != args )
-                        {
-                                argsbody.putAll( args );
-                        }
+                    }
+                    catch( VoldException e )
+                    {
+                        log.error( "Could not handle write request for key " + entry.getKey() + ". ", e );
+                        invalidKeys.put( entry.getKey(), "ERROR: " + e.getMessage() );
+                    }
                 }
-
-                // process each key
-                {
-                        for( Map.Entry< String, List< String > > entry: argsbody.entrySet() )
-                        {
-                                URIKey urikey;
-                                String source;
-                                Key k;
-
-                                // build key
-                                {
-                                        urikey = URIKey.fromURIString( entry.getKey(), enc );
-
-                                        File path_correction = new File( scope + "/" + urikey.getKey().get_scope() );
-
-                                        k = new Key(
-                                                        path_correction.getPath(),
-                                                        urikey.getKey().get_type(),
-                                                        urikey.getKey().get_keyname()
-                                                        );
-
-                                        if( null == urikey.getSource() )
-                                        {
-                                                source = clientIpAddress;
-                                        }
-                                        else
-                                        {
-                                                source = urikey.getSource();
-                                        }
-                                }
-
-                                // handle write request for that key
-                                {
-                                        try
-                                        {
-                                                if( urikey.isDelete() )
-                                                {
-                                                        frontend.delete( source, k );
-                                                }
-                                                else if( urikey.isRefresh() )
-                                                {
-                                                        frontend.refresh( source, k );
-                                                }
-                                                else
-                                                {
-                                                        frontend.insert( source, k, new HashSet< String >( entry.getValue() ) );
-                                                }
-                                        }
-                                        catch( VoldException e )
-                                        {
-                                                log.error( "Could not handle write request for key " + entry.getKey() + ". ", e );
-                                                invalidKeys.put( entry.getKey().toString(), "ERROR: " + e.getMessage() );
-                                                continue;
-                                        }
-                                }
-                        }
-                }
-
-                return new ResponseEntity< Map < String, String > >( invalidKeys, HttpStatus.OK );
+            }
         }
 
-        /**
-        * Handles Get requests.
-        *
-        * This method is used by clients to lookup some keys.
-        *
-        * @param keys The URL arguments of the request.
-        * @return A map of found keys with its associated values.
-        */
-        @RequestMapping( method = RequestMethod.GET )
-        public ResponseEntity< Map< Key, Set< String > > > get(
-                        @RequestParam Map< String, String > keys,
-                        HttpServletRequest request )
+        return new ResponseEntity< Map < String, String > >( invalidKeys, HttpStatus.OK );
+    }
+
+    /**
+     * Handles Get requests.
+     *
+     * This method is used by clients to lookup some keys.
+     *
+     * @param keys The URL arguments of the request.
+     * @param request Request informations
+     * @return A map of found keys with its associated values.
+     */
+    @RequestMapping( method = RequestMethod.GET )
+    public ResponseEntity< Map< Key, Set< String > > > get(
+            @RequestParam Map< String, String > keys,
+            HttpServletRequest request )
+    {
+        // guard
         {
-                // guard
-                {
-                        log.debug( "GET: " + keys.toString() );
+            log.debug( "GET: " + keys.toString() );
 
-                        checkState();
+            checkState();
+        }
+
+        Map< Key, Set< String > > merged_result = new HashMap< Key, Set< String > >();
+
+        // get actual scope
+        String scope;
+        {
+            scope = request.getRequestURI();
+            String removepath = request.getContextPath() + request.getServletPath();
+
+            scope = scope.substring( removepath.length(), scope.length() );
+        }
+
+        // process each key
+        for( Map.Entry< String, String > entry: keys.entrySet() )
+        {
+            URIKey urikey;
+            Key k;
+
+            // build key
+            {
+                urikey = URIKey.fromURIString( entry.getKey(), enc );
+
+                File path_correction = new File( scope + "/" + urikey.getKey().get_scope() );
+
+                k = new Key(
+                        path_correction.getPath(),
+                        urikey.getKey().get_type(),
+                        urikey.getKey().get_keyname()
+                );
+            }
+
+            // lookup and remember result
+            {
+                Map< Key, Set< String > > _result;
+
+                try
+                {
+                    _result = frontend.lookup( k );
                 }
-
-                Map< Key, Set< String > > merged_result = new HashMap< Key, Set< String > >();
-
-                // get actual scope
-                String scope;
+                catch( VoldException e )
                 {
-                        scope = request.getRequestURI();
-                        String removepath = request.getContextPath() + request.getServletPath();
-
-                        scope = scope.substring( removepath.length(), scope.length() );
-                }
-
-                // process each key
-                for( Map.Entry< String, String > entry: keys.entrySet() )
-                {
-                        URIKey urikey;
-                        Key k;
-
-                        // build key
-                        {
-                                urikey = URIKey.fromURIString( entry.getKey(), enc );
-
-                                File path_correction = new File( scope + "/" + urikey.getKey().get_scope() );
-
-                                k = new Key(
-                                                path_correction.getPath(),
-                                                urikey.getKey().get_type(),
-                                                urikey.getKey().get_keyname()
-                                                );
-                        }
-
-                        // lookup and remember result
-                        {
-                                Map< Key, Set< String > > _result; 
-
-                                try
-                                {
-                                        _result = frontend.lookup( k ); 
-                                }
-                                catch( VoldException e )
-                                {
-                                        log.error( "Error on lookup for key " + k + " (" + entry.getKey() + "): ", e );
-                                        continue;
+                    log.error( "Error on lookup for key " + k + " (" + entry.getKey() + "): ", e );
+                    continue;
 /*
                                         Set< String > s = new HashSet< String >();
                                         s.add( e.getMessage() );
@@ -247,33 +247,33 @@ public class RESTController
                                                         merged_result,
                                                         HttpStatus.INTERNAL_SERVER_ERROR );
 */
-                                }
-
-                                // found something
-                                if( null != _result )
-                                {
-                                        merged_result.putAll( _result );
-                                }
-                        }
                 }
 
-                return new ResponseEntity< Map<Key, Set< String > > >( merged_result, HttpStatus.OK );
+                // found something
+                if( null != _result )
+                {
+                    merged_result.putAll( _result );
+                }
+            }
         }
 
-        @ModelAttribute("clientIpAddress")
-        public String populateClientIpAddress( HttpServletRequest request )
-        {
-                return request.getRemoteAddr();
-        }
+        return new ResponseEntity< Map<Key, Set< String > > >( merged_result, HttpStatus.OK );
+    }
 
-        @Autowired
-        public void setFrontend( Frontend frontend )
-        {
-                this.frontend = frontend;
-        }
+    @ModelAttribute("clientIpAddress")
+    public String populateClientIpAddress( HttpServletRequest request )
+    {
+        return request.getRemoteAddr();
+    }
 
-        public void setEnc( String enc )
-        {
-                this.enc = enc;
-        }
+    @Autowired
+    public void setFrontend( Frontend frontend )
+    {
+        this.frontend = frontend;
+    }
+
+    public void setEnc( String enc )
+    {
+        this.enc = enc;
+    }
 }
