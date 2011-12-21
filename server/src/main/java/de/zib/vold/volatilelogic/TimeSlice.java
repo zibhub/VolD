238e04a0479b5eedec26acc3bdfa0d9182b0e324
Jörg Bachmann <bachmann@zib.de>
2011-12-21 11:28:27 +0100

    Removed a System.out.println

--- server/src/main/java/de/zib/vold/volatilelogic/TimeSlice.java
+++ server/src/main/java/de/zib/vold/volatilelogic/TimeSlice.java
@@ -20,7 +20,7 @@ import org.joda.time.Instant;
 
 /**
  * A factor ring implementation.
- * 
+ *
  * This implementation of a factor ring is called TimeSlice, since it provides a
  * method returning an element of the actual factor ring which is associated
  * with the current time.
@@ -32,130 +32,128 @@ import org.joda.time.Instant;
  *
  * @see				SlicedDirectory
  * @see                         Reaper
- * 
+ *
  * @author			Jörg Bachmann (bachmann@zib.de)
  */
 public class TimeSlice
 {
-	private long timeSliceSize;
-	private long numberOfSlices;
+    private long timeSliceSize;
+    private long numberOfSlices;
 
-        /**
-         * Construct a certain factor ring.
-         *
-         * @param tileSliceSize         The size of one time slice in milliseconds.
-         * @param numberOfSlices        The number of elements in the factor ring.
-         */
-	public TimeSlice( long timeSliceSize, long numberOfSlices )
-                throws IllegalArgumentException
-	{
-                setTimeSliceSize( timeSliceSize );
-                setNumberOfSlices( numberOfSlices );
-	}
+    /**
+     * Construct a certain factor ring.
+     *
+     * @param tileSliceSize         The size of one time slice in milliseconds.
+     * @param numberOfSlices        The number of elements in the factor ring.
+     */
+    public TimeSlice( long timeSliceSize, long numberOfSlices )
+            throws IllegalArgumentException
+    {
+        setTimeSliceSize( timeSliceSize );
+        setNumberOfSlices( numberOfSlices );
+    }
 
-        /**
-         * Construct a trivial TimeSlice.
-         *
-         * This constructor intializes the trivial factor ring with one element.
-         */
-        public TimeSlice( )
+    /**
+     * Construct a trivial TimeSlice.
+     *
+     * This constructor intializes the trivial factor ring with one element.
+     */
+    public TimeSlice( )
+    {
+        this.timeSliceSize = 1;
+        this.numberOfSlices = 1;
+    }
+
+    /**
+     * Set the size of one slice in milliseconds.
+     *
+     * @param timeSliceSize The time slice size in milliseconds.
+     */
+    public void setTimeSliceSize( long timeSliceSize )
+    {
+        if( timeSliceSize <= 0 )
         {
-                this.timeSliceSize = 1;
-                this.numberOfSlices = 1;
+            throw new IllegalArgumentException( "Positive values excepted for size of timeslices." );
         }
 
-	/**
-	 * Set the size of one slice in milliseconds.
-         *
-         * @param timeSliceSize The time slice size in milliseconds.
-	 */
-        public void setTimeSliceSize( long timeSliceSize )
-        {
-                if( timeSliceSize <= 0 )
-                {
-                        throw new IllegalArgumentException( "Positive values excepted for size of timeslices." );
-                }
+        this.timeSliceSize = timeSliceSize;
+    }
 
-                this.timeSliceSize = timeSliceSize;
+    /**
+     * Set the number of slices.
+     *
+     * Each slice number will be returned modulo this number.
+     *
+     * @param numberOfSlices The identifier for the factor ring.
+     */
+    public void setNumberOfSlices( long numberOfSlices )
+    {
+        if( numberOfSlices <= 0 )
+        {
+            throw new IllegalArgumentException( "Positive values excepted for number of slices." );
+        }
+        this.numberOfSlices = numberOfSlices;
+    }
+
+    /**
+     * Get the size of one slice in milliseconds.
+     *
+     * @return The time slice size in milliseconds.
+     */
+    public long getTimeSliceSize( )
+    {
+        // guard
+        {
+            if( timeSliceSize <= 0 || numberOfSlices <= 0 )
+            {
+                throw new IllegalStateException( "Tried to operate on TimeSlice while it hat not been initialized properly yet. Setting the size of a timeslice and the number of timeslices is necessary." );
+            }
         }
 
-	/**
-	 * Set the number of slices.
-	 * 
-	 * Each slice number will be returned modulo this number.
-         *
-         * @param numberOfSlices The identifier for the factor ring.
-	 */
-        public void setNumberOfSlices( long numberOfSlices )
+        return this.timeSliceSize;
+    }
+
+    /**
+     * Get the number of slices.
+     *
+     * Each slice number will be returned modulo this number.
+     *
+     * @return The factor ring identifier.
+     */
+    public long getNumberOfSlices( )
+    {
+        // guard
         {
-                if( numberOfSlices <= 0 )
-                {
-                        throw new IllegalArgumentException( "Positive values excepted for number of slices." );
-                }
-                this.numberOfSlices = numberOfSlices;
+            if( timeSliceSize <= 0 || numberOfSlices <= 0 )
+            {
+                throw new IllegalStateException( "Tried to operate on TimeSlice while it hat not been initialized properly yet. Setting the size of a timeslice and the number of timeslices is necessary." );
+            }
         }
 
-	/**
-	 * Get the size of one slice in milliseconds.
-         *
-         * @return The time slice size in milliseconds.
-	 */
-	public long getTimeSliceSize( )
-	{
-                // guard
-                {
-                        if( timeSliceSize <= 0 || numberOfSlices <= 0 )
-                        {
-                                throw new IllegalStateException( "Tried to operate on TimeSlice while it hat not been initialized properly yet. Setting the size of a timeslice and the number of timeslices is necessary." );
-                        }
-                }
+        return numberOfSlices;
+    }
 
-		return this.timeSliceSize;
-	}
+    /**
+     * Returns the slice number for the actual time.
+     *
+     * @note        Its return value may change each time, the method is
+     *              called. Furthermore, the value may no more be valid
+     *              after the method returned.
+     *
+     * @return The current time slice.
+     */
+    public long getActualSlice( )
+    {
+        // guard
+        {
+            if( timeSliceSize <= 0 || numberOfSlices <= 0 )
+            {
+                throw new IllegalStateException( "Tried to operate on TimeSlice while it hat not been initialized properly yet. Setting the size of a timeslice and the number of timeslices is necessary." );
+            }
+        }
 
-	/**
-	 * Get the number of slices.
-	 * 
-	 * Each slice number will be returned modulo this number.
-         *
-         * @return The factor ring identifier.
-	 */
-	public long getNumberOfSlices( )
-	{
-                // guard
-                {
-                        if( timeSliceSize <= 0 || numberOfSlices <= 0 )
-                        {
-                                throw new IllegalStateException( "Tried to operate on TimeSlice while it hat not been initialized properly yet. Setting the size of a timeslice and the number of timeslices is necessary." );
-                        }
-                }
+        long now = Instant.now().getMillis();
 
-		return numberOfSlices;
-	}
-
-	/**
-	 * Returns the slice number for the actual time.
-         *
-         * @note        Its return value may change each time, the method is
-         *              called. Furthermore, the value may no more be valid
-         *              after the method returned.
-         *
-         * @return The current time slice.
-	 */
-	public long getActualSlice( )
-	{
-                // guard
-                {
-                        if( timeSliceSize <= 0 || numberOfSlices <= 0 )
-                        {
-                                throw new IllegalStateException( "Tried to operate on TimeSlice while it hat not been initialized properly yet. Setting the size of a timeslice and the number of timeslices is necessary." );
-                        }
-                }
-
-		long now = Instant.now().getMillis();
-
-                System.out.println( now + ", " + getTimeSliceSize() + ", " + getNumberOfSlices() );
-
-		return ( now / getTimeSliceSize() ) % getNumberOfSlices();
-	}
+        return ( now / getTimeSliceSize() ) % getNumberOfSlices();
+    }
 }
